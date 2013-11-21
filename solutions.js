45de438038b450592b098b75ae6f576a61d81a46
Gary Sieling <gary.sieling@gmail.com>
2013-11-21 17:49:10 -0500

    Update solutions.js

--- solutions.js
+++ solutions.js
@@ -1,3 +1,6 @@
 #1:
 
 var a = 0; for (x = 1; x < 1000; x++) { if ( (x % 3 == 0) || (x % 5 == 0 ) ) { a += x } }
+
+#2:
+var a = 0, b = 1, c = 1; for (; c <= 4000000; ) { if (c % 2 == 0) { a += c; console.log(a) } var d = b + c; b = c; c = d; a }
